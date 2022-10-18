cc845d8790af521800c441aa271e560ef38659ac
Akanksha Ghodke <86144664+Akuu-27@users.noreply.github.com>
2022-10-18 17:45:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,70 @@
+const list = [
+  {
+    id: 1,
+    title: "Burger",
+    price: 89,
+    img: "https://us.123rf.com/450wm/romulogq/romulogq2104/romulogq210400244/169341479-delicious-hamburger-with-french-fries-on-wooden-table-and-black-background-front-view-.jpg?ver=6",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Pizza",
+    price: 98,
+    img: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Noodles",
+    author: "by Om Books Editorial Team  | 25 November 2018",
+    price: 99,
+    img: "https://b.zmtcdn.com/data/pictures/9/19719099/0d94ea05762ef94138eccf2138beb00e_o2_featured_v2.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Idli",
+    price: 40,
+    img: "https://www.vegrecipesofindia.com/wp-content/uploads/2014/05/idli-sambar-1.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Sandwich",
+    price: 30,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVuQV6XfcvsY3uoDOfLp5BPqvoHlzmiU3htbgJjdlNvzbx_UlyTFe8n5FeD08hm1KjGs0&usqp=CAU",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Frid Rice",
+    price: 99,
+    img: "https://i1.wp.com/businessday.ng/wp-content/uploads/2020/09/Untitled-design-2020-09-25T170910.855.png?fit=700%2C400&ssl=1",
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: "Momos",
+    price: 99,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Momo_nepal.jpg/1200px-Momo_nepal.jpg",
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: "Pani-Puri",
+    price: 25,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkcJijZXpwOBLbSolr4IFD2ZQXReiYuIdZVLCKY07CMAdgoBarvBiTeo44SWWwBdwGUng&usqp=CAU",
+    amount: 1,
+  },
+  {
+    id: 9,
+    title: "Bhel",
+    price: 30,
+    img: "https://c.ndtvimg.com/2021-01/0ldb6of8_churumuri_625x300_12_January_21.jpg",
+    amount: 1,
+  },
+  
+ 
+];
+
+export default list;
